9e65fd91e547e13b0ed81642472cbb197c480e7d
KonradDlugosz <56347935+KonradDlugosz@users.noreply.github.com>
2021-02-09 18:45:58 +0000

    Removed unused code

--- App/app.R
+++ App/app.R
@@ -14,9 +14,6 @@ library(shinyalert)
 
 ### Sources ####
 source("data/covid19Data.R")
-source("forecasting/dailyTimeSeriesPlot.R")
-source("forecasting/valueBoxesReturns.R")
-source("forecasting/arimaModel.R")
 source("functions/dashPlots.R")
 source("functions/countryPlots.R")
 source("functions/forecasting.R")
@@ -30,7 +27,6 @@ ui <- dashboardPage(
     sidebarMenu( id = "sideBarMenu",
       menuItem("Dashboard", tabName = "dashboard", icon = icon("virus")),
       menuItem("Plots", tabName = "plots", icon = icon("chart-line")),
-      menuItem("Forecasting", tabName = "forecasting", icon = icon("laptop-code")),
       menuItem("About", tabName = "about", icon = icon("user-secret"))
     )
   ),
@@ -126,31 +122,19 @@ ui <- dashboardPage(
                                                selectInput("country", 
                                                            choices = retrunListOfCountries(), 
                                                            label = "Select country: "),
-                                               radioGroupButtons(
-                                                 inputId = "switchGraphType",
-                                                 label = "Choose graph :", 
+                                               radioGroupButtons(inputId = "switchGraphType",label = "Choose graph :", 
                                                  choices = c(`<i class='fa fa-bar-chart'></i>` = "bar", `<i class='fa fa-line-chart'></i>` = "line"),
-                                                 justified = TRUE,
-                                                 size = "lg"
+                                                 justified = TRUE,size = "lg"
                                                ),
-                                               radioGroupButtons(
-                                                 inputId = "switchData",
-                                                 label = "Choose data :", 
+                                               radioGroupButtons(inputId = "switchData",label = "Choose data :", 
                                                  choices = c(`<i class="fas fa-head-side-mask"></i>` = "cases",`<i class="fas fa-skull-crossbones"></i>`= "deaths"),
-                                                 justified = TRUE,
-                                                 size = "lg"
+                                                 justified = TRUE,size = "lg"
                                                ),
                                                column(8 ,h3("Exponential Moving Average: ")),
                                                column(4 ,br(),switchInput(inputId = "movingAverage", value = FALSE)),
-                                               knobInput(
-                                                 inputId = "daysToForecast",
-                                                 label = "Days to forecast:",
-                                                 value = 14,
-                                                 min = 0,
-                                                 max = 30,
-                                                 displayPrevious = TRUE,
-                                                 fgColor = "#428BCA",
-                                                 inputColor = "#428BCA"
+                                               knobInput(inputId = "daysToForecast", label = "Days to forecast:",
+                                                 value = 14, min = 0, max = 30, displayPrevious = TRUE,
+                                                 fgColor = "#428BCA",inputColor = "#428BCA"
                                                ),
                                                dropdown(style = "jelly", icon = icon("question"), color = "primary", align = "right",
                                                         p(id ="info-label","Forecast done using Feed-forward neural networks."),
@@ -158,54 +142,8 @@ ui <- dashboardPage(
                                                ), 
                                  ),
                              )),
-                  )
-              )),
+                  ))),
       #### Plots ENDS ----------------------------------------------------------###
-      ### FORECASTING PAGE STARTS ----------------------------------------------###
-      tabItem(tabName = "forecasting",
-              fluidRow(
-                column(width = 8,
-                       #Value Boxes
-                       valueBoxOutput("Cases"),
-                       valueBoxOutput("Deaths"),
-                       valueBoxOutput("Tests")
-                )
-              ),
-              fluidRow(
-                column(width = 12, 
-                       box(
-                         title = "Daily Time Series plot", solidHeader = TRUE, collapsible = TRUE, status = "primary", br(), width = 12,
-                         sidebarPanel(
-                           h3("Customise: "), br(),
-                           selectInput("plotData", choices = c("New cases", "Deaths"), label = "Select data: "),
-                           selectInput("plotType", choices = c("Daily", "Commutative"), label = "Select plot type: "),
-                           h4("Moving Averages: "),
-                           checkboxInput("checkBoxMA", label = "Simple Moving Average", value = FALSE),
-                           checkboxInput("checkBoxEMA", label = "Exponential Moving Average", value = FALSE),
-                           p("Data source: API Gov Uk")
-                         ),
-                         mainPanel(
-                           plotOutput("plotOutput",  height = "auto")
-                         )),
-                       
-                       box(
-                         title = "ARIMA Model", solidHeader = TRUE, collapsible = TRUE, status = "primary", br(), width = 12,
-                         sidebarPanel(
-                           h3("Forecast from ARIMA: "), br(),
-                           selectInput("plotData2", choices = c("New cases", "Deaths"), label = "Select data: "),
-                           sliderInput("movingAvergeNum", "Moving average period:",
-                                       min = 0, max = 50, value = 7),
-                           sliderInput("numberOfForecastDays", "Number of days to forecast:",
-                                       min = 0, max = 50, value = 7),
-                           p("As the number grows, the accuracy decreases.")
-                         ),
-                         mainPanel(
-                           plotOutput("arimaOutput",  height = "500px")
-                         )),
-                )
-              )
-      ),
-      
       ### ABOUT PAGE STARTS ----------------------------------------------------###
       tabItem(tabName = "about")
       
@@ -231,7 +169,6 @@ server <- function(input, output, session) {
   output$casesHighChart <- renderHighchart({
     pieControler("cases")
   })
-  #casesMap("mymap")
   
   # Main Buttons 
   observeEvent(input$btn_totalCases, {
@@ -346,64 +283,6 @@ server <- function(input, output, session) {
   })
   
   # Plots Ends -----------------------------------------------------------------
-  # Forecasting start ----------------------------------------------------------
-  
-  output$Cases <- renderValueBox({
-    valueBox(
-      totalDailyCasesUK(), "Total Cases", icon = icon("briefcase-medical"),
-      color = "orange"
-    )
-  })
-  
-  output$Deaths <- renderValueBox({
-    valueBox(
-      totalDailyDeathsUK(), "Total deaths ", icon = icon("skull-crossbones"),
-      color = "red"
-    )
-  })
-  
-  output$Tests <- renderValueBox({
-    valueBox(
-      totalNumberOfTests(), "Total tests", icon = icon("vials"),
-      color = "green"
-    )
-  })
-  
-  # Plot mechanism selection render
-  output$plotOutput <- renderPlot({
-    if(input$plotData == "New cases"){
-      
-      if(input$plotType == "Daily"){
-        dailyCasesTimeSeriesPlot(input$checkBoxMA, input$checkBoxEMA)
-      }
-      else if(input$plotType == "Commutative"){    
-        dailyCasesLinear()
-      }
-    }
-    else if(input$plotData == "Deaths"){
-      
-      if(input$plotType == "Daily"){
-        dailyDeathsTimeSeriesPlot(input$checkBoxMA, input$checkBoxEMA)
-      }
-      else if(input$plotType == "Commutative"){
-        dailyCommutativeDeathsPlot()
-      }
-    }
-  })
-  
-  
-  # ARIMA Mechanism Selection render
-  output$arimaOutput <- renderPlot({
-    if(input$plotData2 == "New cases"){
-      dailyCasesARIMA(input$movingAvergeNum, input$numberOfForecastDays)
-    }
-    else if(input$plotData2 == "Deaths"){
-      dailyDeathsARIMA(input$movingAvergeNum, input$numberOfForecastDays)
-    }
-    
-  })
-  
-  # Forecasting End ------------------------------------------------------------
   
 }
 
